ee53c797aa1ddfa9e1f92163f4388e457122e060
ChaninR <cr@manaosoftware.com>
2023-03-07 14:32:41 +0700

    [update]: adjust UI

--- user/lib/pages/login.dart
+++ user/lib/pages/login.dart
@@ -16,57 +16,61 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
       body: SafeArea(
-          child: Column(
-        children: [
-          SizedBox(
-            width: 100,
-            height: 100,
-            child: Image.asset('images/MyIcon.jpg'),
-          ),
-          Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(labelText: 'Email'),
-                    validator: (value) {
-                      if (value!.isEmpty || !emailForm.hasMatch(value)) {
-                        return 'Invalid Email';
-                      }
-                      return null;
-                    },
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(labelText: 'Password'),
-                    validator: (value) {
-                      if (value!.isEmpty || value.length < 8) {
-                        return 'Invalid Password';
-                      }
-                      return null;
-                    },
-                  ),
-                  ElevatedButton(
-                      onPressed: (() {
-                        if (_formKey.currentState!.validate()) {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(content: Text('Processing')));
+          child: Container(
+        margin: const EdgeInsets.only(top: 40, left: 30, right: 30, bottom: 40),
+        child: Column(
+          children: [
+            SizedBox(
+              width: 100,
+              height: 100,
+              child: Image.asset('images/MyIcon.jpg'),
+            ),
+            Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(labelText: 'Email'),
+                      validator: (value) {
+                        if (value!.isEmpty || !emailForm.hasMatch(value)) {
+                          return 'Invalid Email';
                         }
-                      }),
-                      child: const Text('Login'))
-                ],
-              )),
-          TextButton(
-              onPressed: (() {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (_) => const RegisterPage()));
-              }),
-              child: const Text('Register'))
-        ],
+                        return null;
+                      },
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(labelText: 'Password'),
+                      validator: (value) {
+                        if (value!.isEmpty || value.length < 8) {
+                          return 'Invalid Password';
+                        }
+                        return null;
+                      },
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 40, left: 20, right: 20, bottom: 20),
+                      child: ElevatedButton(
+                          onPressed: (() {
+                            if (_formKey.currentState!.validate()) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(content: Text('Processing')));
+                            }
+                          }),
+                          child: const Text('เข้าสู่ระบบ')),
+                    )
+                  ],
+                )),
+            TextButton(
+                onPressed: (() {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const RegisterPage()));
+                }),
+                child: const Text('สมัครสมาชิก'))
+          ],
+        ),
       )),
     );
   }
