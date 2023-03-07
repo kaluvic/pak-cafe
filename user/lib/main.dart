0c676455c4461640fc8093ceda9b4421cc764143
Cold3x <rqkung@gmail.com>
2023-03-07 16:13:50 +0700

    setup firebase

--- user/lib/main.dart
+++ user/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pak_user/firebase_options.dart';
 import 'package:pak_user/pages/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
