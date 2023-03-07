98295101781cd057ed81d0232adbafc3d0af9fa5
AcediaSan <Boss450018@gmail.com>
2023-03-07 14:20:51 +0700

    update order card

--- admin/lib/pages/order_page.dart
+++ admin/lib/pages/order_page.dart
@@ -15,13 +15,66 @@ class _OrderPageState extends State<OrderPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Order Page')),
-      body: Center(
-        child: Column(
-          children: const [
-            OrderCardWidget(),
-            OrderCardWidget(),
-          ],
-        )
+      body: SingleChildScrollView(
+        child: Container(
+            margin: EdgeInsets.only(top: 20.0, bottom: 20.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Expanded(
+                      child: Divider(
+                        indent: 20.0,
+                        endIndent: 10.0,
+                        thickness: 1,
+                      ),
+                    ),
+                    Text(
+                      "ออเดอร์ที่กำลังดำเนินการ",
+                      style: TextStyle(color: Colors.blueGrey),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        indent: 10.0,
+                        endIndent: 20.0,
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+
+                const OrderCardWidget(),
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Expanded(
+                      child: Divider(
+                        indent: 20.0,
+                        endIndent: 10.0,
+                        thickness: 1,
+                      ),
+                    ),
+                    Text(
+                      "ออเดอร์ที่ยังไม่ได้รับ",
+                      style: TextStyle(color: Colors.blueGrey),
+                    ),
+                    Expanded(
+                      child: Divider(
+                        indent: 10.0,
+                        endIndent: 20.0,
+                        thickness: 1,
+                      ),
+                    ),
+                  ],
+                ),
+
+                const OrderCardWidget(),
+                const OrderCardWidget(),
+              
+              ],
+            )),
       ),
     );
   }
--- admin/lib/widgets/order_card.dart
+++ admin/lib/widgets/order_card.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class OrderCardWidget extends StatelessWidget {
+class OrderCardWidget extends StatefulWidget {
   const OrderCardWidget({super.key});
+
+  @override
+  State<OrderCardWidget> createState() => _OrderCardWidgetState();
+}
+
+class _OrderCardWidgetState extends State<OrderCardWidget> {
+  List<String> menuList = ['t1', 't2', 't3'];
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -10,41 +18,100 @@ class OrderCardWidget extends StatelessWidget {
         clipBehavior: Clip.hardEdge,
         child: InkWell(
           splashColor: Colors.blue.withAlpha(30),
-          onTap: () {
-            debugPrint('Card tapped.');
-          },
+          onTap: () => showDialog(
+            context: context,
+            builder: (BuildContext context) => AlertDialog(
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "Username",
+                  ),
+                  Text(
+                    "Order ID",
+                  ),
+                ],
+              ),
+              content: const Text('เมนูที่สั่ง'),
+              actions: <Widget>[
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'รับออเดอร์'),
+                  child: const Text('รับออเดอร์'),
+                ),
+                TextButton(
+                  onPressed: () => Navigator.pop(context, 'ยกเลิก'),
+                  child: const Text('ยกเลิก'),
+                ),
+              ],
+            ),
+          ),
           child: SizedBox(
             width: 300,
-            height: 100,
-            child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text(
-                      "Username",
-                    ),
-                    Text(
-                      "Order ID",
-                    ),
-                  ],
-                ),
-                const Text("OrderList"),
                 Container(
-                  
+                  margin: const EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Username",
+                      ),
+                      Text(
+                        "Order ID",
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  child: orderlist(menuList.length),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: const [
-                      ElevatedButton(onPressed: null, child: Text("order")),
-                      ElevatedButton(onPressed: null, child: Text("cancle")),
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text("รับออเดอร์"),
+                      ),
+                      ElevatedButton(
+                          onPressed: () {}, child: const Text("ยกเลิก")),
                     ],
-                  )
-                ),
+                  ),
+                )
               ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  orderlist(int range) {
+    if (range <= 2) {
+      if (range == 1) {
+        return Text(menuList[0]);
+      } else {
+        return Column(
+          children: [
+            Text(menuList[0]),
+            Text(menuList[1]),
+          ],
+        );
+      }
+    } else {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(menuList[0]),
+          Text(menuList[1]),
+          Text("... มีอีก ${range - 2} รายการ"),
+        ],
+      );
+    }
   }
 }
